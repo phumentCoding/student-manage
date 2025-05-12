d20d31021623a7dfa7b0a0f09415653dbd61c758
phumentCoding <phumentceo@gmail.com>
2025-05-12 14:24:13 +0700

    update

--- connect.php
+++ connect.php
@@ -2,7 +2,7 @@
    $conn = mysqli_connect('127.0.0.1','root','','etec-database',3307);
 
    if(!$conn){
-      die("Error -> ". mysqli_connect_errno());
+      die("Error -> ". mysqli_connect_error());
    }
 
 
