50dbd495fb0f79625fbd61a30138f30703a6881c
phumentCoding <phumentceo@gmail.com>
2025-05-12 14:23:41 +0700

    update

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 <?php 
    $conn = mysqli_connect('127.0.0.1','root','','etec-database',3307);
 
-   
-
-   if($conn){
-      echo "Connection successfully";
-   }else{
-      echo "Fail: " . mysqli_connect_error();
+   if(!$conn){
+      die("Error -> ". mysqli_connect_errno());
    }
 
 
